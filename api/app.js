6163adbbf9ec5790e1a54f95188ff1e705f01f96
Sumit-Ks1 <sumit127624@gmail.com>
2025-01-17 15:14:16 +0530

    Updated cors

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "https://seva-hub.vercel.app/",
+    origin: "https://seva-hub.vercel.app",
     credentials: true
 }))
 
